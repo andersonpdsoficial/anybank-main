196cac1e004107463561fef60a273657afd1a317
andersonpdsoff <apdsengsoft@gmail.com>
2025-04-11 12:05:05 -0400

    implementação df de AuthContext

---
+++ src/app/context/AuthContext.tsx
@@ -0,0 +1,44 @@
+import { createContext, ReactNode, useEffect, useState } from "react"
+import { supabase } from "../../infra/supabase/config"
+import { Session } from "@supabase/supabase-js"
+
+interface AuthContextType {
+    session: Session | null;
+    logout: () => Promise<void>;
+}
+
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+
+    const [session, setSession] = useState<Session | null>(null)
+
+    useEffect(() => {
+        supabase.auth.getSession()
+            .then(({ data: { session } }) => {
+                setSession(session)
+                console.log('getSession', session)
+            })
+
+        const {
+            data: { subscription },
+        } = supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session)
+            console.log('onAuthStateChange', session)
+        })
+
+        return () => subscription.unsubscribe()
+    }, [])
+
+    const logout = async () => {
+        await supabase.auth.signOut()
+        setSession(null)
+    }
+
+    return (
+        <AuthContext.Provider value={{ session, logout }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
