a36065f8c6710c2bdc5261080d649c7b811a5d04
Anderson Pereira da Silva <andersonpds17042000@gmail.com.com>
2025-04-15 22:26:33 -0400

    ajsute no na boas vindas

--- src/presentation/Header/AutheticadedActionList.tsx
+++ src/presentation/Header/AutheticadedActionList.tsx
@@ -25,7 +25,7 @@ export const AutheticadedActionList = () => {
     return (
         <List>
             <ListItem>
-                BOas Vindas
+                Boas Vindas
             </ListItem>
             <ListItem>
                 <IconAvatar />
