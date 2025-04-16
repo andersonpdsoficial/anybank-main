b3952b76d87adaf3d1931e6656b240d9e7126c5e
Anderson Pereira da Silva <andersonpds17042000@gmail.com.com>
2025-04-15 22:19:56 -0400

    removendo esibição dos consoles

--- src/app/context/AuthContext.tsx
+++ src/app/context/AuthContext.tsx
@@ -19,7 +19,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
         supabase.auth.getSession()
           .then(({ data: { session } }) => {
               setSession(session)
-              console.log('getSession', session)
+              
           })
           .finally(() => setLoading(false))
 
@@ -27,7 +27,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
             data: { subscription },
         } = supabase.auth.onAuthStateChange((_event, session) => {
             setSession(session)
-            console.log('onAuthStateChange', session)
+            
         })
 
         return () => subscription.unsubscribe()
